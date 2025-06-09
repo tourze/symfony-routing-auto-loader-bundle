a92db40ae6500babe7c98b5b79520831a41f03f9
github-ci <you@example.com>
2025-06-09 22:28:59 +0800

    新增 `tourze/bundle-dependency` 依赖，`RoutingAutoLoaderBundle` 实现 `BundleDependencyInterface` 接口并添加依赖声明逻辑。

--- src/RoutingAutoLoaderBundle.php
+++ src/RoutingAutoLoaderBundle.php
@@ -2,8 +2,16 @@
 
 namespace Tourze\RoutingAutoLoaderBundle;
 
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
 
-class RoutingAutoLoaderBundle extends Bundle
+class RoutingAutoLoaderBundle extends Bundle implements BundleDependencyInterface
 {
+    public static function getBundleDependencies(): array
+    {
+        return [
+            FrameworkBundle::class => ['all' => true],
+        ];
+    }
 }
